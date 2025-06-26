0b2b96ae4e3c7a7b52c69b42c67d41162efab31c
vivekokta <vivek.giri@okta.com>
2025-06-26 19:58:38 +0530

    routing added

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ import { join } from 'path';
 
 config({ path: join(import.meta.dirname, 'testenv') });
 
-export const base_url = process.env.URL;
+export const base_url = process.env.BASE_URL;
 export const port = process.env.PORT;
 export const client_id = process.env.CLIENT_ID;
+export const client_secret = process.env.CLIENT_SECRET;
 export const kid = process.env.KID;
 export const scopes = process.env.SCOPES;
--- index.js
+++ index.js
@@ -7,20 +7,32 @@ const app = express();
 const port = 8080;
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(import.meta.dirname, "views"));
-
+app.set('views', path.join(import.meta.dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 const results = await getSystemLogs();
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
 app.get('/dashboard', (req, res) => {
   console.log(results);
   res.render('dashboard', { results });
+});
+
+app.get('/save-details', (req, res) => {
+  res.render('saveCreds');
+});
+
+app.post('/submit', (req, res) => {
+  const { orgDomain } = req.body;
+  console.log(orgDomain);
+  // console.log('Received inputs:', input1, input2, input3);
+
+  res.redirect('/');
 });
